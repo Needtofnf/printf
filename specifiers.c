b3c9beafdea53477fae43b8322b7d4af44c07c4a
Thulani <nalebaniokiy99@gmail.com>
2022-04-16 11:52:42 -0700

    be

--- specifiers.c
+++ specifiers.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-
+* main - Entry point
+*
+* Return: Always 0
+*/
 int main(void)
 {
-printf("Hello world!\n");
+int number_One = 10;
+int number_two = 20;
+
+printf("The output is :%d\n", number_One);
+printf("The output is :%i\n", number_two);
+
 return (0);
 }
 
